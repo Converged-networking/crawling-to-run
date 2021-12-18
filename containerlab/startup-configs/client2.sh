8d48afcf54dc0d2fc6594866eae9fa415e9a57f1
Marvin Kuurstra <marvin@kuurstra.com>
2021-12-18 16:28:07 +0100

    Add LACP rate fast and better transmit policy

--- containerlab/startup-configs/client2.sh
+++ containerlab/startup-configs/client2.sh
@@ -6,6 +6,8 @@ ip link set eth1 down
 ip link set eth1 master bond0
 ip link set eth2 down
 ip link set eth2 master bond0
+ip link set dev bond0 type bond xmit_hash_policy layer3+4
+ip link set dev bond0 type bond lacp_rate fast
 ip link set bond0 up
 
 ip address add 10.0.12.5/24 dev bond0
